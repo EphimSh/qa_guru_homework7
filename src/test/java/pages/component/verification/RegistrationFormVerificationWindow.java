7e16d6d3ff1416327ebf88ca48cb8ba0289ff481
EphimSh <ephimizm@gmail.com>
2023-06-07 23:52:48 +0900

    RegistrationFormVerificationWindow updated

--- src/test/java/pages/component/verification/RegistrationFormVerificationWindow.java
+++ src/test/java/pages/component/verification/RegistrationFormVerificationWindow.java
@@ -9,52 +9,52 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
-public class VerificationWindow {
+public class RegistrationFormVerificationWindow {
     SelenideElement tableResponsive = $(".table-responsive");
 
 
-    public VerificationWindow verifyStudentFullName(String fullName) {
+    public RegistrationFormVerificationWindow verifyStudentFullName(String fullName) {
         tableResponsive.$(byText("Student Name")).sibling(0).shouldHave(text(fullName));
         return  this;
     }
-    public VerificationWindow verifyStudentEmail(String email) {
+    public RegistrationFormVerificationWindow verifyStudentEmail(String email) {
         tableResponsive.$(byText("Student Email")).sibling(0).shouldHave(text(email));
         return  this;
     }
-    public VerificationWindow verifyStudentGender(String gender) {
+    public RegistrationFormVerificationWindow verifyStudentGender(String gender) {
         tableResponsive.$(byText("Gender")).sibling(0).shouldHave(text(gender));
         return  this;
     }
-    public VerificationWindow verifyStudentMobileNumber(String mobNumber) {
+    public RegistrationFormVerificationWindow verifyStudentMobileNumber(String mobNumber) {
         tableResponsive.$(byText("Mobile")).sibling(0).shouldHave(text(mobNumber));
         return  this;
     }
-    public VerificationWindow verifyStudentBirthDate(String day, String month, String year) {
+    public RegistrationFormVerificationWindow verifyStudentBirthDate(String day, String month, String year) {
         String birthDate = day + " " + (month.substring(0, 1).toUpperCase() + month.substring(1)) + "," + year;
         tableResponsive.$(byText("Date of Birth")).sibling(0).shouldHave(text(birthDate));
         return  this;
     }
-    public VerificationWindow verifyStudentSubject(String Subject) {
+    public RegistrationFormVerificationWindow verifyStudentSubject(String Subject) {
         tableResponsive.$(byText("Subjects")).sibling(0).shouldHave(text(Subject));
         return  this;
     }
-    public VerificationWindow verifyStudentHobbies(String hobbies) {
+    public RegistrationFormVerificationWindow verifyStudentHobbies(String hobbies) {
         String formattedHobbies = Arrays.stream(hobbies.split("\\s*,\\s*|\\s+"))
                 .map(String::trim)
                 .collect(Collectors.joining(", "));
         tableResponsive.$(byText("Hobbies")).sibling(0).shouldHave(text(formattedHobbies));
         return  this;
     }
-    public VerificationWindow verifyStudentUploadedPicture(String filePath) {
+    public RegistrationFormVerificationWindow verifyStudentUploadedPicture(String filePath) {
         String imgName = filePath.substring(filePath.lastIndexOf("/") + 1);
         tableResponsive.$(byText("Picture")).sibling(0).shouldHave(text(imgName));
         return  this;
     }
-    public VerificationWindow verifyStudentAddress(String address) {
+    public RegistrationFormVerificationWindow verifyStudentAddress(String address) {
         tableResponsive.$(byText("Address")).sibling(0).shouldHave(text(address));
         return  this;
     }
-    public VerificationWindow verifyStudentStateAndCity(String state, String city) {
+    public RegistrationFormVerificationWindow verifyStudentStateAndCity(String state, String city) {
         String stateAndCity = state + " " + city;
         tableResponsive.$(byText("State and City")).sibling(0).shouldHave(text(stateAndCity));
         return  this;
