d72e1efc7a5c2799087d023bf462c6dd750cebcd
EphimSh <ephimizm@gmail.com>
2023-06-07 19:50:01 +0900

    RegistrationPage added

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,121 @@
+package pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+import pages.component.CalendarComponent;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+    private final SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userMobNumber = $("#userNumber"),
+            dateOfBirthInput = $("#dateOfBirthInput"),
+            subjectsContainer = $("#subjectsContainer"),
+            uploadPictureBTN = $("#uploadPicture"),
+            currentAddressField = $("#currentAddress"),
+            stateDropDown = $("#state"),
+            cityDropDown = $("#city"),
+            submitButton = $("#submit");
+
+
+
+
+
+    private final ElementsCollection hobbiesList = $$("#hobbiesWrapper [class*='custom-checkbox']");
+
+
+
+    public RegistrationPage openPage() {
+        open("automation-practice-form");
+        return this;
+    }
+
+
+    public RegistrationPage setFirstName(String name) {
+        firstNameInput.setValue(name);
+        return this;
+    }
+
+    public RegistrationPage setLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+        return this;
+    }
+
+    public RegistrationPage setEmail(String email) {
+        userEmailInput.setValue(email);
+        return this;
+    }
+
+    public RegistrationPage selectGender(String gender) {
+        genderWrapper.$(byTextCaseInsensitive(gender)).click();
+        return this;
+    }
+
+    public RegistrationPage setUserMobNumber(String tenDigitsMobileNumber) {
+        userMobNumber.setValue(tenDigitsMobileNumber);
+        return this;
+    }
+
+
+    public RegistrationPage subjectSet(String subject) {
+        subjectsContainer.click();
+        subjectsContainer.$("#subjectsInput").setValue(subject).sendKeys(Keys.ARROW_DOWN);
+        subjectsContainer.$("#subjectsInput").pressEnter();
+        return this;
+    }
+
+    public RegistrationPage pickHobbies(String hobby) {
+        List<String> hobbies = new ArrayList<>(Arrays.asList(hobby.split("\\s*,\\s*")));
+        for (String h : hobbies) {
+            hobbiesList.find(text(h)).click();
+
+        }
+        return this;
+
+    }
+
+    public RegistrationPage uploadPicture(String filePath) {
+        uploadPictureBTN.uploadFile(new File(filePath));
+        return this;
+    }
+
+    public RegistrationPage setCurrentAddress(String address) {
+        currentAddressField.setValue(address);
+        return this;
+    }
+
+    public RegistrationPage selectStateAndCity(String state, String city) {
+        stateDropDown.click();
+        $(byText(state)).click();
+        $(byText(state)).click();
+        cityDropDown.click();
+        cityDropDown.$(byText(city)).click();
+        return this;
+    }
+
+    public RegistrationPage pressSubmitButton() {
+        submitButton.click();
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        dateOfBirthInput.click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+}
---
+++ src/test/java/pages/component/CalendarComponent.java
@@ -0,0 +1,24 @@
+package pages.component;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTextCaseInsensitive;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CalendarComponent {
+
+    private final SelenideElement datePicker = $("#dateOfBirth-wrapper");
+
+
+    public CalendarComponent setDate(String day, String month, String year) {
+
+        datePicker.$$("[class*='year-select'] option").find(text(year)).click();
+        datePicker.$("div[class*='month']").$(byTextCaseInsensitive(month)).click();
+        //here we need change first letter of our month 👇 to uppercase
+        datePicker.$$(" [aria-label*='" + (month.substring(0, 1).toUpperCase() + month.substring(1)) + "'] ")
+                .findBy(text(day)).click();
+        return this;
+    }
+}
