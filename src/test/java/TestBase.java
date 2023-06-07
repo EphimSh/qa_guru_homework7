300dfe0e2e2056320d5327df829a2ce1bfd4a243
EphimSh <ephimizm@gmail.com>
2023-06-07 19:50:16 +0900

    TestBase added

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,15 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    public static String imgFilePath = "src/test/resources/haha.jpg";
+    @BeforeAll
+    static void init(){
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+
+        Configuration.baseUrl = "https://demoqa.com/";
+    }
+}
