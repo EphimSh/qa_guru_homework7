d858778321f4c00e310c1e43f440c45e3f6226ab
Ephim Shaposhnikov <37134016+EphimSh@users.noreply.github.com>
2023-06-08 14:15:03 +0900

    unnecessary imports removed
    javascriptexecutor

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,8 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
-
 public class TestBase {
 
 
